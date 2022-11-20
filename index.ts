58a61320fd212881960f0a95d8e6942665fece79
CarlonBaird <carlyestrella@live.com>
2022-11-20 16:09:32 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
+import { Observable, of } from 'rxjs';
 
+//So, the 'of' function returns an Observable, so we can subscribe to it.
+of('Alice', 'Ben', 'Charlie').subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Completed'),
+});
 
+const names$ = new Observable<string>((subscriber) => {
+  subscriber.next('Alice');
+  subscriber.next('Ben');
+  subscriber.next('Charlie');
+  subscriber.complete();
+});
+
+names$.subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Completed'),
+});
