9664e9de6c20e0dcbf2e2d38bf4dc79255aa514f
CarlonBaird <carlyestrella@live.com>
2022-11-20 16:20:27 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,20 +1,32 @@
 import { Observable, of } from 'rxjs';
 
 //So, the 'of' function returns an Observable, so we can subscribe to it.
-of('Alice', 'Ben', 'Charlie').subscribe({
-  next: (value) => console.log(value),
-  complete: () => console.log('Completed'),
-});
+// of('Alice', 'Ben', 'Charlie').subscribe({
+//   next: (value) => console.log(value),
+//   complete: () => console.log('Completed'),
+// });
 
 //the above implementation is the same as follows:
-const names$ = new Observable<string>((subscriber) => {
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
-});
+// const names$ = new Observable<string>((subscriber) => {
+//   subscriber.next('Alice');
+//   subscriber.next('Ben');
+//   subscriber.next('Charlie');
+//   subscriber.complete();
+// });
 
-names$.subscribe({
+// names$.subscribe({
+//   next: (value) => console.log(value),
+//   complete: () => console.log('Completed'),
+// });
+
+function myOf(...args: string[]): Observable<string> {
+  return new Observable<string>((subsriber) => {
+    args.forEach((value) => subsriber.next(value));
+    subsriber.complete();
+  });
+}
+
+myOf('Alice', 'Ben', 'Charlie').subscribe({
   next: (value) => console.log(value),
   complete: () => console.log('Completed'),
 });
